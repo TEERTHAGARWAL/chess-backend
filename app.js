a6a6774e177db91ec48db2f3b73b1662781d0e47
ChangeS99 <64601689+ChangeS99@users.noreply.github.com>
2021-04-09 21:08:51 +0530

    added origin

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const app = express()
 
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 /**
  * Backend flow:
  * - check to see if the game ID encoded in the URL belongs to a valid game session in progress. 
